dcda13c2d7e91794eb49ba5357d74392a8869f21
Mahdi Masaeli <masaelimahdiwork@gmail.com>
2024-06-18 21:41:43 +0330

    Fix: fix series loading page design

--- src/app/series/[id]/loading.tsx
+++ src/app/series/[id]/loading.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Loading(){
     return (
-      <div className="max-w-[500px] mx-auto absolute top-[50%] left-[50%]">
+      <div className="max-w-[500px] mx-auto flex flex-col justify-center items-center min-h-screen">
         <p className="playfair-display text-white">Loading ...</p>
         <Image 
         src='/assets/images/loading.gif'
